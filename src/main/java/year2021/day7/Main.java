5393d0b4ebfb17bb349dbdbe5defd7af9000ff9d
Vries <j.devries1@dictu.nl>
2021-12-07 08:42:13 +0100

    day 7 2021

--- src/main/java/year2021/day7/Main.java
+++ src/main/java/year2021/day7/Main.java
@@ -4,7 +4,9 @@ import base.Base;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main extends Base {
     private static final String INPUT = "2021/day7-input.txt";
@@ -30,11 +32,38 @@ public class Main extends Base {
 
     @Override
     public void part1() throws IOException {
-
+        System.out.println(calculateFuel(false));
     }
 
     @Override
     public void part2() throws IOException {
+        System.out.println(calculateFuel(true));
+    }
 
+    private int calculateFuel(boolean part2) {
+        List<Integer> positions = Arrays.stream(instructions.get(0).split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        int min = positions.stream().mapToInt(v -> v).min().getAsInt();
+        int max = positions.stream().mapToInt(v -> v).max().getAsInt();
+
+        int minFuel = Integer.MAX_VALUE;
+
+        for (int i = min; i <= max; i++) {
+            int fuel = 0;
+
+            for (int crab : positions) {
+                int diff = Math.abs(crab - i);
+                int diffFuel = 0;
+                if (part2) {
+                    for (int n = 0; n < diff; n++) {
+                        diffFuel += n + 1;
+                    }
+                }
+                fuel += part2 ? diffFuel : diff;
+            }
+
+            minFuel = fuel < minFuel ? fuel : minFuel;
+        }
+
+        return minFuel;
     }
 }
