8b758396c6527e4842f242bbe6c48f1b6fcfff07
jurgen <jurgen.de.vries@ordina.nl>
2022-12-04 12:42:05 +0100

    day 4

--- src/main/java/year2022/Day4.java
+++ src/main/java/year2022/Day4.java
@@ -9,8 +9,8 @@ public class Day4 extends Base {
 
     public static void main(String[] args) throws IOException {
         Day4 main = new Day4();
-//        main.mainMethod(INPUT);
-//        main.prepareInput();
+        main.mainMethod(INPUT);
+        main.prepareInput();
         System.out.println("PART1:");
         long start = System.currentTimeMillis();
         main.part1();
@@ -22,11 +22,35 @@ public class Day4 extends Base {
 
     @Override
     public void part1() throws IOException {
-
+        int matchingPairs = determineOverlap(true);
+        System.out.println(matchingPairs);
     }
 
     @Override
     public void part2() throws IOException {
+        int matchingPairs = determineOverlap(false);
+        System.out.println(matchingPairs);
+    }
 
+    private int determineOverlap(boolean fullOverlap) {
+        int matchingPairs = 0;
+        for (String instruction : instructions) {
+            int start1 = Integer.parseInt(instruction.split(",")[0].split("-")[0]);
+            int end1 = Integer.parseInt(instruction.split(",")[0].split("-")[1]);
+            int start2 = Integer.parseInt(instruction.split(",")[1].split("-")[0]);
+            int end2 = Integer.parseInt(instruction.split(",")[1].split("-")[1]);
+
+            if (fullOverlap) {
+                if ((start1 <= start2 && end1 >= end2) || (start2 <= start1 && end2 >= end1)) {
+                    matchingPairs++;
+                }
+            }  else {
+                if ((end1 >= start2 && end1 <= end2) || (end2 >= start1 && end2 <= end1)) {
+                    matchingPairs++;
+                }
+            }
+        }
+
+        return matchingPairs;
     }
 }
