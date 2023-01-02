e1956ac1a2435cd501c780a36ac705243281dbb5
vriesj13 <j.devries1@dictu.nl>
2023-01-02 09:26:28 +0100

    day 23 part 2

--- src/main/java/year2022/Day23.java
+++ src/main/java/year2022/Day23.java
@@ -1,20 +1,19 @@
 package year2022;
 
 import base.Base;
+import base.utils.Utils;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Day23 extends Base {
-    private static final String INPUT = "2022/day23-test.txt";
+    private static final String INPUT = "2022/day23-input.txt";
     private static final int N = 0;
     private static final int S = 1;
     private static final int W = 2;
     private static final int E = 3;
     List<Elf> elfs;
+    List<Elf> propesedElfs;
     private static int[][][] directions;
     private static int[][] north;
     private static int[][] east;
@@ -53,6 +52,7 @@ public class Day23 extends Base {
         while ((part1 && round < 10) || (!part1 && elfMoved)) {
             elfMoved = false;
             Set<String> seen = new HashSet<>();
+            propesedElfs = new ArrayList<>();
             // check possible movement
             for (Elf elf : elfs) {
                 // look in all directions
@@ -68,41 +68,49 @@ public class Day23 extends Base {
                 checks[W] = canMoveWest;
                 checks[E] = canMoveEast;
 
+                Elf pElf = new Elf(elf.id, elf.x, elf.y);
                 if ((canMoveNorth && canMoveEast && canMoveSouth && canMoveWest) || (!canMoveNorth && !canMoveEast && !canMoveSouth && !canMoveWest)) {
-                    //elf.proposedPos = new Coord(elf.currentPos.x, elf.currentPos.y);
+                    // nothing to do here
                 } else {
                     boolean moved = false;
+
                     for (int add = 0; add < 4; add++) {
                         int check = (round + add) % 4;
 
                         if (checks[check]) {
                             switch (check) {
                                 case N:
-                                    elf.proposedPos.x = elf.currentPos.x;
-                                    elf.proposedPos.y = elf.currentPos.y - 1;
+                                    pElf = new Elf(elf.id, elf.x, elf.y - 1);
                                     moved = true;
                                     break;
                                 case S:
-                                    elf.proposedPos.x = elf.currentPos.x;
-                                    elf.proposedPos.y = elf.currentPos.y + 1;
+                                    pElf = new Elf(elf.id, elf.x, elf.y + 1);
                                     moved = true;
                                     break;
                                 case W:
-                                    elf.proposedPos.x = elf.currentPos.x - 1;
-                                    elf.proposedPos.y = elf.currentPos.y;
+                                    pElf = new Elf(elf.id, elf.x - 1, elf.y);
                                     moved = true;
                                     break;
                                 case E:
-                                    elf.proposedPos.x = elf.currentPos.x + 1;
-                                    elf.proposedPos.y = elf.currentPos.y;
+                                    pElf = new Elf(elf.id, elf.x + 1, elf.y);
                                     moved = true;
                                     break;
+                                default:
+                                    pElf = new Elf(elf.id, elf.x, elf.y);
                             }
-                            if (!seen.contains("" + elf.proposedPos.x + "," + elf.proposedPos.y)) {
-                                seen.add("" + elf.proposedPos.x + "," + elf.proposedPos.y);
+                            if (!seen.contains("" + pElf.x + "," + pElf.y)) {
+                                seen.add("" + pElf.x + "," + pElf.y);
                             } else {
-                                elf.proposedPos.x = elf.currentPos.x;
-                                elf.proposedPos.y = elf.currentPos.y;
+                                int newX = pElf.x;
+                                int newY = pElf.y;
+                                Optional<Elf> resetElf = propesedElfs.stream().filter(e -> e.x == newX && e.y == newY).findFirst();
+                                pElf.x = elf.x;
+                                pElf.y = elf.y;
+                                if (resetElf.isPresent()) {
+                                    Elf oriElf = elfs.stream().filter(e -> e.id == resetElf.get().id).findFirst().get();
+                                    resetElf.get().x = oriElf.x;
+                                    resetElf.get().y = oriElf.y;
+                                }
                             }
                         }
                         if (moved) {
@@ -111,37 +119,37 @@ public class Day23 extends Base {
                         }
                     }
                 }
+                propesedElfs.add(pElf);
             }
 
             // actually move if no other elf is trying this move
-            for (Elf elf : elfs) {
-                elf.currentPos.x = elf.proposedPos.x;
-                elf.currentPos.y = elf.proposedPos.y;
-            }
+            elfs = new ArrayList<>(propesedElfs);
+
             if (elfMoved) {
                 round++;
             }
         }
-        int minX = elfs.stream().mapToInt(e -> e.currentPos.x).min().getAsInt();
-        int maxX = elfs.stream().mapToInt(e -> e.currentPos.x).max().getAsInt();
-        int minY = elfs.stream().mapToInt(e -> e.currentPos.y).min().getAsInt();
-        int maxY = elfs.stream().mapToInt(e -> e.currentPos.y).max().getAsInt();
+        int minX = elfs.stream().mapToInt(e -> e.x).min().getAsInt();
+        int maxX = elfs.stream().mapToInt(e -> e.x).max().getAsInt();
+        int minY = elfs.stream().mapToInt(e -> e.y).min().getAsInt();
+        int maxY = elfs.stream().mapToInt(e -> e.y).max().getAsInt();
         int width = maxX - minX + 1;
         int height = maxY - minY + 1;
-//            int offsetY = minY < 0 ? 0 - minY: 0 - minY;
-//            int offsetX = minX < 0 ? 0 - minX: 0 - minX;
-//            String[][] grid = new String[height][width];
-//
-//            for (int y = 0; y < height; y++) {
-//                for (int x = 0; x < width; x++) {
-//                    grid[y][x] = ".";
-//                }
-//            }
-//
-//            for (Elf elf : elfs) {
-//                grid[elf.currentPos.y + offsetY][elf.currentPos.x + offsetX] = "#";
-//            }
-        //Utils.printGrid(width, height, grid);
+
+        int offsetY = minY < 0 ? 0 - minY: 0 - minY;
+        int offsetX = minX < 0 ? 0 - minX: 0 - minX;
+        String[][] grid = new String[height][width];
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                grid[y][x] = ".";
+            }
+        }
+
+        for (Elf elf : elfs) {
+            grid[elf.y + offsetY][elf.x + offsetX] = "#";
+        }
+        Utils.printGrid(width, height, grid);
 
         System.out.println("Round [" + (round+1) + "]: " + (width * height - elfs.size()));
     }
@@ -150,7 +158,7 @@ public class Day23 extends Base {
         boolean canMove = true;
         int[][] windDir = directions[dirNum];
         for (int[] dir : windDir) {
-            if (elfs.stream().filter(e -> e.id != elf.id && e.currentPos.x == elf.currentPos.x + dir[1] && e.currentPos.y == elf.currentPos.y + dir[0]).findFirst().isPresent()) {
+            if (elfs.stream().filter(e -> e.id != elf.id && e.x == elf.x + dir[1] && e.y == elf.y + dir[0]).findFirst().isPresent()) {
                 canMove = false;
                 break;
             }
@@ -165,7 +173,7 @@ public class Day23 extends Base {
             String[] row = instructions.get(y).split("");
             for (int x = 0; x < row.length; x++) {
                 if ("#".equals(row[x])) {
-                    Elf elf = new Elf(elfId++, new Coord(x, y));
+                    Elf elf = new Elf(elfId++, x, y);
                     elfs.add(elf);
                 }
             }
@@ -179,27 +187,13 @@ public class Day23 extends Base {
 
     class Elf {
         private int id;
-        private Coord currentPos;
-        private Coord proposedPos;
-
-        public Elf(int id, Coord currentPos) {
-            this.id = id;
-            this.currentPos = currentPos;
-            this.proposedPos = new Coord();
-        }
-    }
-
-    class Coord {
         private int x;
         private int y;
 
-        public Coord(int x, int y) {
+        public Elf(int id, int x, int y) {
+            this.id = id;
             this.x = x;
             this.y = y;
-        }
-
-        public Coord() {
-
         }
     }
 
