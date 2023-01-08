b204a5e1415863b1d2eb6a4d35f44b82b4038cd3
vriesj13 <j.devries1@dictu.nl>
2023-01-08 15:48:59 +0100

    day 22 example

--- src/main/java/year2022/Day16.java
+++ src/main/java/year2022/Day16.java
@@ -1,7 +1,6 @@
 package year2022;
 
 import base.Base;
-import year2021.day23.Main;
 
 import java.io.IOException;
 import java.util.*;
@@ -18,8 +17,8 @@ public class Day16 extends Base {
         main.mainMethod(INPUT);
         main.prepareInput();
         long start = System.currentTimeMillis();
-        System.out.println("PART1:");
-        main.part1();
+//        System.out.println("PART1:");
+//        main.part1();
         System.out.println("PART2:");
         main.part2();
         long end = System.currentTimeMillis();
@@ -48,38 +47,113 @@ public class Day16 extends Base {
     public void part1() throws IOException {
         List<Valve> valves = readValves();
         Valve currentValve = valves.stream().filter(v -> v.id.equals("AA")).findFirst().get();
+        State startState = new State(valves, currentValve, 0, 30);
+        move(startState);
+
+        int topScore = scores.stream().mapToInt(x -> x).max().orElse(0);
+        System.out.println(topScore);
+
+//        System.out.println("Part2:");
+//        // get state from known states
+//
+//        List<Map.Entry<String, Integer>> endStates = knownStates.entrySet().stream().filter(k -> "0".equals(k.getKey().split("-")[2])).collect(Collectors.toList());
+//        for (Map.Entry<String, Integer> endState : endStates) {
+//            // find inverted state at time 0
+//
+//            // find all closed valves
+//            // find all opened valves
+//        }
+//
+//        String state = knownStates.entrySet().stream().filter(x -> x.getValue() == topScore).findFirst().get().getKey();
+//        // GS:closed;CB:closedTP:closedFI:closedWV:closedEA:openAT:closedWS:closedMP:closedGE:closedSA:openNI:closedGO:closedIT:closedNG:closedRD:closedLR:openTO:closedWF:closedYD:closedXR:closedKF:closedOO:closedHG:closedCT:closedDA:closedYY:closedVW:closedRC:closedPL:closedTH:openQN:openXE:closedXP:closedAX:closedEM:closedNR:closedYG:openPM:closedAY:closedGJ:closedLC:closedUD:closedAA:closedOM:closedWB:closedGW:openNA:closedXX:closedZO:closedKB:open-AY-0 -> {Integer@1018} 1366
+//
+//
+//        // for all states find the inverted state and combine values
+//
+//        // highest total value wins
+//
+//
+//        for (Valve valve : valves) {
+//            for (String valveState : state.split(";")) {
+//                if (valve.id.equals(valveState.split(":")[0])) {
+//                    valve.open = valveState.split(":")[1].equals("open");
+//                    break;
+//                }
+//            }
+//        }
+//        System.out.println(state);
+//        scores = new HashSet<>();
+//        State elephantStartState = new State(valves, currentValve, 0, 26);
+//        move(elephantStartState);
+//        int topElephantScore = scores.stream().mapToInt(x -> x).max().orElse(0);
+//        System.out.println(topElephantScore);
+//
+//        System.out.println(topElephantScore+ topScore);
+    }
+
+    @Override
+    public void part2() throws IOException {
+        List<Valve> valves = readValves();
+        Valve currentValve = valves.stream().filter(v -> v.id.equals("AA")).findFirst().get();
         State startState = new State(valves, currentValve, 0, 26);
         move(startState);
 
         int topScore = scores.stream().mapToInt(x -> x).max().orElse(0);
         System.out.println(topScore);
 
-        System.out.println("Part2:");
-        // get state from known states
-        String state = knownStates.entrySet().stream().filter(x -> x.getValue() == topScore).findFirst().get().getKey();
-        // GS:closed;CB:closedTP:closedFI:closedWV:closedEA:openAT:closedWS:closedMP:closedGE:closedSA:openNI:closedGO:closedIT:closedNG:closedRD:closedLR:openTO:closedWF:closedYD:closedXR:closedKF:closedOO:closedHG:closedCT:closedDA:closedYY:closedVW:closedRC:closedPL:closedTH:openQN:openXE:closedXP:closedAX:closedEM:closedNR:closedYG:openPM:closedAY:closedGJ:closedLC:closedUD:closedAA:closedOM:closedWB:closedGW:openNA:closedXX:closedZO:closedKB:open-AY-0 -> {Integer@1018} 1366
 
-        for (Valve valve : valves) {
-            for (String valveState : state.split(";")) {
-                if (valve.id.equals(valveState.split(":")[0])) {
-                    valve.open = valveState.split(":")[1].equals("open");
-                    break;
-                }
+        int maxVal = Integer.MIN_VALUE;
+//        get all endstates from known states
+        List<Map.Entry<String, Integer>> endStates = knownStates.entrySet().stream().filter(k -> "0".equals(k.getKey().split("-")[2])).collect(Collectors.toList());
+        for (Map.Entry<String, Integer> endState : endStates) {
+            // get open/closed:
+            String openClosedVales = endState.getKey().split("-")[0];
+            String inverted = openClosedVales.replaceAll("0", "x").replaceAll("1", "0").replaceAll("x", "1");
+
+            // find max score for current openClosedValves situation:
+            int maxCurrentEndState = endStates.stream().filter(e -> openClosedVales.equals(e.getKey().split("-")[0])).collect(Collectors.toList()).stream().mapToInt(x -> x.getValue()).max().getAsInt();
+
+            // find inverted states at time 0
+            List<Map.Entry<String, Integer>> invertedStates = knownStates.entrySet().stream().filter(e -> inverted.equals(e.getKey().split("-")[0])).collect(Collectors.toList());
+
+            // get highest inverted
+            OptionalInt maxInvert = invertedStates.stream().mapToInt(x -> x.getValue()).max();
+            if (maxInvert.isPresent()) {
+
+                // combine values
+                int total = maxInvert.getAsInt() + endState.getValue();
+
+                maxVal = total > maxVal ? total : maxVal;
             }
+            // clean up endstates
         }
-        System.out.println(state);
-        scores = new HashSet<>();
-        State elephantStartState = new State(valves, currentValve, 0, 26);
-        move(elephantStartState);
-        int topElephantScore = scores.stream().mapToInt(x -> x).max().orElse(0);
-        System.out.println(topElephantScore);
-
-        System.out.println(topElephantScore+ topScore);
-    }
-
-    @Override
-    public void part2() throws IOException {
-
+        System.out.println(maxVal);
+//
+//        String state = knownStates.entrySet().stream().filter(x -> x.getValue() == topScore).findFirst().get().getKey();
+//        // GS:closed;CB:closedTP:closedFI:closedWV:closedEA:openAT:closedWS:closedMP:closedGE:closedSA:openNI:closedGO:closedIT:closedNG:closedRD:closedLR:openTO:closedWF:closedYD:closedXR:closedKF:closedOO:closedHG:closedCT:closedDA:closedYY:closedVW:closedRC:closedPL:closedTH:openQN:openXE:closedXP:closedAX:closedEM:closedNR:closedYG:openPM:closedAY:closedGJ:closedLC:closedUD:closedAA:closedOM:closedWB:closedGW:openNA:closedXX:closedZO:closedKB:open-AY-0 -> {Integer@1018} 1366
+//
+//
+//        // for all states find the inverted state and combine values
+//
+//        // highest total value wins
+//
+//
+//        for (Valve valve : valves) {
+//            for (String valveState : state.split(";")) {
+//                if (valve.id.equals(valveState.split(":")[0])) {
+//                    valve.open = valveState.split(":")[1].equals("open");
+//                    break;
+//                }
+//            }
+//        }
+//        System.out.println(state);
+//        scores = new HashSet<>();
+//        State elephantStartState = new State(valves, currentValve, 0, 26);
+//        move(elephantStartState);
+//        int topElephantScore = scores.stream().mapToInt(x -> x).max().orElse(0);
+//        System.out.println(topElephantScore);
+//
+//        System.out.println(topElephantScore+ topScore);
     }
 
     private void move(State currentState) {
@@ -156,7 +230,8 @@ public class Day16 extends Base {
     private String getStateString(List<Valve> valves, int time, String current) {
         StringBuilder stateString = new StringBuilder();
         for (Valve valve : valves) {
-            stateString.append(valve.id + ":" + (valve.open ? "open" : "closed") + ";");
+            //stateString.append(valve.id + ":" + (valve.open ? "open" : "closed") + ";");
+            stateString.append(valve.open ? "1" : "0");
         }
         stateString.append("-" + current + "-" + time);
         return stateString.toString();
--- src/main/java/year2022/Day22.java
+++ src/main/java/year2022/Day22.java
@@ -4,10 +4,11 @@ import base.Base;
 import base.utils.Utils;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.BiFunction;
 
 public class Day22 extends Base {
-    private static final String INPUT = "2022/day22-input.txt";
+    private static final String INPUT = "2022/day22-test.txt";
 
     private static int width;
     private static int height;
@@ -24,9 +25,10 @@ public class Day22 extends Base {
         main.prepareInput();
         long start = System.currentTimeMillis();
         System.out.println("PART1:");
-        main.part1();
+        //main.part1();
         System.out.println("PART2:");
-        main.part2();
+        //main.part2();
+        main.part2b();
         long end = System.currentTimeMillis();
         System.out.println("duration: " + (end - start));
 
@@ -65,9 +67,10 @@ public class Day22 extends Base {
         }
         passPhrase = instructions.get(instructions.size() - 1);
         // 0       1      2      3
-        // right - down - left - right
+        // right - down - left - up
         directions = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         Utils.printGrid(width, height, grid);
+        currentDirection = 0;
 //        System.out.println(passPhrase);
     }
 
@@ -321,6 +324,161 @@ public class Day22 extends Base {
         System.out.println(result);
     }
 
+    public void part2b() throws IOException {
+        readInput();
+        int[] distances = Arrays.stream(passPhrase.split("\\D")).mapToInt(x -> Integer.parseInt(x)).toArray();
+        String[] directionChanges = passPhrase.split("\\d+");
+        currentDirection = 0;
+
+        // create cube
+        Map<Integer, String> connectionsA = new HashMap<>();
+        connectionsA.put(0, "F2");
+        connectionsA.put(3, "B1");
+        connectionsA.put(2, "C1");
+        Face a = new Face("A", 8, 11, 0, 3, connectionsA, (Integer x, Integer y) -> 15, (Integer x, Integer y) -> 11 - y,
+                (Integer x, Integer y) -> x, (Integer x, Integer y) -> y,
+                (Integer x, Integer y) -> y + 4, (Integer x, Integer y) -> 4,
+                (Integer x, Integer y) -> 11 - x, (Integer x, Integer y) -> 4
+        );
+
+        Map<Integer, String> connectionsB = new HashMap<>();
+        connectionsB.put(1, "E3");
+        connectionsB.put(2, "F3");
+        connectionsB.put(3, "A1");
+        Face b = new Face("B", 0, 3, 4, 7, connectionsB, (Integer x, Integer y) -> x, (Integer x, Integer y) -> y,
+                (Integer x, Integer y) -> 11 - x, (Integer x, Integer y) -> 11,
+                (Integer x, Integer y) -> 19 - y, (Integer x, Integer y) -> 11,
+                (Integer x, Integer y) -> 11 - x, (Integer x, Integer y) -> 0
+        );
+
+        Map<Integer, String> connectionsC = new HashMap<>();
+        connectionsC.put(1, "E0");
+        connectionsC.put(3, "A0");
+        Face c = new Face("C", 4, 7, 4, 7, connectionsC, (Integer x, Integer y) -> x, (Integer x, Integer y) -> y,
+                (Integer x, Integer y) -> 8, (Integer x, Integer y) -> 15 - x,
+                (Integer x, Integer y) -> x, (Integer x, Integer y) -> y,
+                (Integer x, Integer y) -> 8, (Integer x, Integer y) -> x - 4
+        );
+
+        Map<Integer, String> connectionsD = new HashMap<>();
+        connectionsD.put(0, "F1");
+        Face d = new Face("D", 8, 11, 4, 7, connectionsD, (Integer x, Integer y) -> 19 - y, (Integer x, Integer y) -> 8,
+                (Integer x, Integer y) -> x, (Integer x, Integer y) -> y,
+                (Integer x, Integer y) -> x, (Integer x, Integer y) -> y,
+                (Integer x, Integer y) -> x, (Integer x, Integer y) -> y
+        );
+
+        Map<Integer, String> connectionsE = new HashMap<>();
+        connectionsE.put(1, "B3");
+        connectionsE.put(2, "C3");
+        Face e = new Face("E", 8, 11, 8, 11, connectionsE, (Integer x, Integer y) -> x, (Integer x, Integer y) -> y,
+                (Integer x, Integer y) -> 11 - x, (Integer x, Integer y) -> 7,
+                (Integer x, Integer y) -> 15 - y, (Integer x, Integer y) -> 7,
+                (Integer x, Integer y) -> x, (Integer x, Integer y) -> y
+        );
+
+        Map<Integer, String> connectionsF = new HashMap<>();
+        connectionsF.put(0, "A2");
+        connectionsF.put(1, "B0");
+        connectionsF.put(3, "D2");
+        Face f = new Face("F", 12, 15, 8, 11, connectionsF, (Integer x, Integer y) -> 11, (Integer x, Integer y) -> 11 - y,
+                (Integer x, Integer y) -> 0, (Integer x, Integer y) -> 19 - x,
+                (Integer x, Integer y) -> x, (Integer x, Integer y) -> y,
+                (Integer x, Integer y) -> 11, (Integer x, Integer y) -> 19 - x
+        );
+        List<Face> faces = new ArrayList<>();
+        faces.add(a);
+        faces.add(b);
+        faces.add(c);
+        faces.add(d);
+        faces.add(e);
+        faces.add(f);
+        Cube cube = new Cube(faces);
+
+
+        for (int i = 0; i < distances.length; i++) {
+            int dist = distances[i];
+            // move
+            moveB(dist, current, cube);
+            currentDirection = changeDir(i, directionChanges, currentDirection);
+        }
+        System.out.println();
+        Utils.printGrid(width, height, grid);
+        System.out.println("Done: [" + (current.x + 1) + "," + (current.y + 1) + "]");
+        int resultX = current.x + 1;
+        int resultY = current.y + 1;
+        int result = 1000 * resultY + 4 * resultX + currentDirection;
+        System.out.println(result);
+    }
+
+    private void moveB(int dist, Coord c, Cube cube) {
+        boolean canMove = true;
+        int moves = 0;
+        while (canMove && moves < dist) {
+            Coord newC = getNewCoordB(c, cube);
+            if (c.x != newC.x || c.y != newC.y) {
+                grid[c.y][c.x] = currentDirection == 0 ? ">" : currentDirection == 1 ? "v" : currentDirection == 2 ? "<" : "^";
+                c.x = newC.x;
+                c.y = newC.y;
+                moves++;
+            } else {
+                canMove = false;
+            }
+        }
+    }
+
+    private Coord getNewCoordB(Coord c, Cube cube) {
+        Coord newC = new Coord(c.x + directions[currentDirection][1], c.y + directions[currentDirection][0]);
+        if (newC.x < 0 || newC.x >= width || newC.y < 0 || newC.y >= height || " ".equals(grid[newC.y][newC.x])) {
+            // outside grid or empty space -> wrap around get first space in next cube face
+
+            // determine current cube face
+            Face currentFace = c.x >= cube.faces.get(0).xMin && c.x <= cube.faces.get(0).xMax && c.y >= cube.faces.get(0).yMin && c.y <= cube.faces.get(0).yMax ? cube.faces.get(0) :
+                    c.x >= cube.faces.get(1).xMin && c.x <= cube.faces.get(1).xMax && c.y >= cube.faces.get(1).yMin && c.y <= cube.faces.get(1).yMax ? cube.faces.get(1) :
+                    c.x >= cube.faces.get(2).xMin && c.x <= cube.faces.get(2).xMax && c.y >= cube.faces.get(2).yMin && c.y <= cube.faces.get(2).yMax ? cube.faces.get(2) :
+                    c.x >= cube.faces.get(3).xMin && c.x <= cube.faces.get(3).xMax && c.y >= cube.faces.get(3).yMin && c.y <= cube.faces.get(3).yMax ? cube.faces.get(3) :
+                    c.x >= cube.faces.get(4).xMin && c.x <= cube.faces.get(4).xMax && c.y >= cube.faces.get(4).yMin && c.y <= cube.faces.get(4).yMax ? cube.faces.get(4) : cube.faces.get(5);
+
+            if (currentFace.connections.get(currentDirection) != null) {
+                // complex move to connecting face
+                String connection = currentFace.connections.get(currentDirection);
+                String newFaceId = connection.split("")[0];
+                currentDirection = Integer.parseInt(connection.split("")[1]);
+                Face newFace = cube.faces.stream().filter(f -> newFaceId.equals(f.id)).findFirst().get();
+                switch (currentDirection) {
+                    case 0:
+                        newC.x = currentFace.detX0.apply(c.x, c.y);
+                        newC.y = currentFace.detY0.apply(c.x, c.y);
+                        break;
+                    case 1:
+                        newC.x = currentFace.detX1.apply(c.x, c.y);
+                        newC.y = currentFace.detY1.apply(c.x, c.y);
+                        break;
+                    case 2:
+                        newC.x = currentFace.detX2.apply(c.x, c.y);
+                        newC.y = currentFace.detY2.apply(c.x, c.y);
+                        break;
+                    default:
+                        newC.x = currentFace.detX3.apply(c.x, c.y);
+                        newC.y = currentFace.detY3.apply(c.x, c.y);
+                }
+            } else {
+                // impossible move!
+                System.out.println("ERROR! impossible move!!");
+            }
+            return newC;
+        } else if ("#".equals(grid[newC.y][newC.x])) {
+            // hits wall -> return current coordinate (no move)
+            return c;
+        } else if (".".equals(grid[newC.y][newC.x])  || ">".equals(grid[newC.y][newC.x]) || "v".equals(grid[newC.y][newC.x]) || "<".equals(grid[newC.y][newC.x]) || "^".equals(grid[newC.y][newC.x])) {
+            // '.' -> just move to next open space (move)
+            return newC;
+        }
+        System.out.println("Can we get here???");
+
+        return null;
+    }
+
     private int changeDir(int index, String[] directionChanges, int currentDirection) {
         if (index + 1 < directionChanges.length) {
             switch (directionChanges[index + 1]) {
@@ -335,6 +493,65 @@ public class Day22 extends Base {
             }
         }
         return currentDirection;
+    }
+
+    class Cube {
+        private List<Face> faces;
+
+        public Cube(List<Face> faces) {
+            this.faces = faces;
+        }
+    }
+
+    class Face {
+        private String id;
+        private int xMin;
+        private int xMax;
+        private int yMin;
+        private int yMax;
+        private String[][] faceGrid;
+        private Map<Integer, String> connections;
+        private BiFunction<Integer, Integer, Integer> detX0;
+        private BiFunction<Integer, Integer, Integer> detY0;
+        private BiFunction<Integer, Integer, Integer> detX1;
+        private BiFunction<Integer, Integer, Integer> detY1;
+        private BiFunction<Integer, Integer, Integer> detX2;
+        private BiFunction<Integer, Integer, Integer> detY2;
+        private BiFunction<Integer, Integer, Integer> detX3;
+        private BiFunction<Integer, Integer, Integer> detY3;
+
+        public Face(String id, int xMin, int xMax, int yMin, int yMax, Map<Integer, String> connections, BiFunction<Integer, Integer, Integer> detX0, BiFunction<Integer, Integer, Integer> detY0, BiFunction<Integer, Integer, Integer> detX1, BiFunction<Integer, Integer, Integer> detY1, BiFunction<Integer, Integer, Integer> detX2, BiFunction<Integer, Integer, Integer> detY2, BiFunction<Integer, Integer, Integer> detX3, BiFunction<Integer, Integer, Integer> detY3) {
+            this.id = id;
+            this.xMin = xMin;
+            this.xMax = xMax;
+            this.yMin = yMin;
+            this.yMax = yMax;
+            this.connections = connections;
+            this.detX0 = detX0;
+            this.detY0 = detY0;
+            this.detX1 = detX1;
+            this.detY1 = detY1;
+            this.detX2 = detX2;
+            this.detY2 = detY2;
+            this.detX3 = detX3;
+            this.detY3 = detY3;
+        }
+
+        public Face(String id, int xMin, int xMax, int yMin, int yMax, Map<Integer, String> connections) {
+            this.id = id;
+            this.xMin = xMin;
+            this.xMax = xMax;
+            this.yMin = yMin;
+            this.yMax = yMax;
+            this.connections = connections;
+
+            this.faceGrid = new String[yMax - yMin][xMax - xMin];
+            for (int y = yMin; y <= yMax; y++) {
+                for (int x = xMin; x <= xMax; x++) {
+                    this.faceGrid[y][x] = grid[y][x];
+                }
+            }
+        }
     }
 
     class Coord {
